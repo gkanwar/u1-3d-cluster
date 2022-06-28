b8cec11e34699c957d848b396abbbcda8225bcda
Gurtej Kanwar <gurtej@mit.edu>
2022-06-28 16:38:39 +0200

    Adds UBELIX submit scripts for CUDA metropolis

---
+++ cuda_metropolis/fire_L80_sweep.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export L=80
+export n_iter=1000000
+export n_therm=10000
+export n_skip_meas=100
+
+ALL_E2=($(seq 0.30 0.05 1.80))
+ALL_SEED=($(seq 224419 225419))
+for i in "${!ALL_E2[@]}"; do
+    e2="${ALL_E2[i]}"
+    seed="${ALL_SEED[i]}"
+    sbatch --time 00:30:00 --export=ALL,e2=${e2},seed=${seed} run_one_job.sh
+done
---
+++ cuda_metropolis/run_one_job.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#SBATCH --job-name=u1_3d_cluster
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:gtx1080ti:1
+#SBATCH --qos=job_gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+
+if [ -z "${e2+x}" ]; then
+    echo "Must set e2"
+    exit
+fi
+if [ -z "${L+x}" ]; then
+    echo "Must set L"
+    exit
+fi
+
+if [ -z "${n_iter+x}" ]; then
+    echo "Must set n_iter"
+    exit
+fi
+if [ -z "${n_therm+x}" ]; then
+    echo "Must set n_therm"
+    exit
+fi
+if [ -z "${n_skip_meas+x}" ]; then
+    echo "Must set n_skip_meas"
+    exit
+fi
+if [ -z "${seed+x}": ]; then
+    echo "Must set seed"
+    exit
+fi
+
+
+cd ${HOME}/u1_3d_cluster/cuda_metropolis
+module load CUDA
+srun --ntasks-per-node=1 \
+    ./u1_3d_cuda --n_iter=${n_iter} --n_therm=${n_therm} --n_skip_meas=${n_skip_meas} \
+    --seed=${seed} --e2=${e2} --L=${L} \
+    --out_prefix=obs_trace_${e2}_L${L}_cuda
